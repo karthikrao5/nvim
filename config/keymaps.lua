1cdae11ec3da721d3387a5a65058a45468ed834e
Thales <me@karthikrao.dev>
2023-08-20 17:41:22 -0400

    .

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -1,1 +1,1 @@
-vim.wo.relativenumber = true
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,21 +2,24 @@
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-return {{
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    keys = {
-      {'<leader>fg', "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
-      {'<leader>ff', "<cmd>Telescope find_files<cr>", desc = "Find file"},
-    }
-}, {
-    "telescope.nvim",
-    dependencies = {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        config = function()
-            require("telescope").load_extension("fzf")
-        end
-    }
-}}
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file" },
+		},
+	},
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
+	},
+}
